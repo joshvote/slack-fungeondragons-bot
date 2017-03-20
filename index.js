9ac56a9564a5d4592bd807e2c9e8790f32039e1f
joshvote <Josh.Vote@csiro.au>
2017-03-21 07:51:06 +0800

    beepboop echo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Botkit = require('botkit');
+var BeepBoop = require('beepboop-botkit');
 
 var PORT = process.env.PORT
 if (!PORT) {
@@ -20,7 +21,7 @@ var controller = Botkit.slackbot({
 });
 
 console.log('Starting in Beep Boop multi-team mode')
-require('beepboop-botkit').start(controller, { debug: true })
+BeepBoop.start(controller, { debug: true })
 var markov = require('markov')(1);
 
 var COMMAND_MAPPINGS = {
@@ -36,16 +37,6 @@ controller.setupWebserver(PORT, function(err, webserver) {
     }
     // Setup our slash command webhook endpoints
     controller.createWebhookEndpoints(webserver)
-});
-
-controller.hears('.*', ['message_received'], function(bot, message) {    
-    console.log("Got message type:" + message.type);
-    console.log("bot.rtm:" + bot.rtm);
-    console.log("message contents:" + JSON.stringify(message));
-    
-    if (message.text) {
-        markov.seed(message.text);
-    }
 });
 
 controller.on('slash_command', function(bot, message) {
@@ -162,6 +153,8 @@ function handle_echo(bot, message, params) {
     if (params) {
         var obj = null;
         switch (params[0]) {
+            case "beepboop":
+                obj = BeepBoop;
             case "controller":
                 obj = controller;
             case "message":
